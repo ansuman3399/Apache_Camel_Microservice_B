a5068d82ad16abf8b1a9edac04d0ecb9cc42f63a
Ansuman Mohanty <ansumanm20@gmail.com>
2021-07-25 02:15:10 +0530

    Initial commit

---
+++ src/test/java/com/apache/CamelB/CamelBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache.CamelB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
